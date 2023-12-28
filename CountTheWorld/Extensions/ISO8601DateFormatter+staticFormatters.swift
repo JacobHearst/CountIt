c4effc0b9c6eae0905fbfe78cdf4bd8dda9a6ce7
Jacob Hearst <jhearst@protonmail.ch>
2023-12-28 07:54:08 -0600

    Fix graph labels

--- CountTheWorld/Extensions/ISO8601DateFormatter+staticFormatters.swift
+++ CountTheWorld/Extensions/ISO8601DateFormatter+staticFormatters.swift
@@ -9,25 +9,25 @@ extension ISO8601DateFormatter {
 
     static let withIdentifiableDay = {
         let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withDay, .withMonth, .withYear]
+        formatter.formatOptions = [.withDay, .withMonth, .withYear, .withDashSeparatorInDate]
         return formatter
     }()
 
     static let withIdentifiableWeek = {
         let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withWeekOfYear, .withYear]
+        formatter.formatOptions = [.withWeekOfYear, .withYear, .withDashSeparatorInDate]
         return formatter
     }()
 
     static let withIdentifiableMonth = {
         let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withMonth, .withYear]
+        formatter.formatOptions = [.withMonth, .withYear, .withDashSeparatorInDate]
         return formatter
     }()
 
     static let withIdentifiableYear = {
         let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withYear]
+        formatter.formatOptions = [.withYear, .withDashSeparatorInDate]
         return formatter
     }()
 }
